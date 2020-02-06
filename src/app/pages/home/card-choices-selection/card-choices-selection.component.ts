85ae731ddef2a62796178fc3c71040095f440930
Banti Shaw -X (banshaw - HCL TECHNOLOGIES LTD at Cisco) <banshaw@cisco.com>
2020-02-06 10:57:03 +0530

    Added changes for second cards

--- src/app/pages/home/card-choices-selection/card-choices-selection.component.ts
+++ src/app/pages/home/card-choices-selection/card-choices-selection.component.ts
@@ -12,28 +12,6 @@ export class CardChoicesSelectionComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.firstRow.push(this.ChoiceData[0]);
-    this.firstRow.push(this.ChoiceData[1]);
-    this.secondRow.push(this.ChoiceData[2]);
-    this.secondRow.push(this.ChoiceData[3]);
-    this.secondRow.push(this.ChoiceData[4]);
-
-    this.firstRow[0].size = 4;
-    this.firstRow[0].width  = '80px';
-    this.firstRow[0].left = '-10px';
-    this.firstRow[1].size = 8;
-    this.firstRow[1].width  = '170px';
-
-    this.firstRow.map(item => {
-      item.height = '140px';
-    });
-
-    this.secondRow.map(item => {
-      item.size = 4;
-      item.height = '90px';
-      item.width = '80px';
-      item.top =  '-22px';
-    });
   };
 
 }
